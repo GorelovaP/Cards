12ef9025981efdfeb416f566810487f4e1a1811d
Nikylin96 <aponosyonok@yandex.by>
2022-10-17 20:33:54 +0300

    add : Header.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,17 @@
 import React from 'react'
 
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import { GlobalStyles } from '../common/styledComponents/globalStyles'
-import { StyledMainWrapper } from '../common/styledComponents/styledWrappers'
 import { theme } from '../common/styledComponents/theme'
-import { SignUpPage } from '../features/signUpPage/SignUpPage'
-import { SignInPage } from '../features/singInPage/SignInPage'
 
 function App() {
-  // it's for me...please don't delete
-  const shouldRedirect = false
-
   return (
     <div>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
-        <StyledMainWrapper>
-          <Routes>
-            <Route index path={'/cards'} element={<div>main</div>} />
-            <Route
-              path={'signup'}
-              element={shouldRedirect ? <Navigate replace to="signin" /> : <SignUpPage />}
-            />
-            <Route path={'signin'} element={<SignInPage />} />
-            <Route path={'/*'} element={<div>404</div>} />
-          </Routes>
-        </StyledMainWrapper>
+        <HashRouter></HashRouter>
       </ThemeProvider>
     </div>
   )
---
+++ src/common/styledComponents/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+import logo from '../../assets/images/logo.svg'
+
+import { StyledPrimaryFormButton } from './styledButtons'
+
+export const Header = () => {
+  return (
+    <StyledSignUpForm>
+      <img src={logo} alt="logo" />
+      <div>
+        <StyledPrimaryFormButton text={'sign in'} />
+      </div>
+    </StyledSignUpForm>
+  )
+}
+
+export const StyledSignUpForm = styled.div`
+  position: fixed;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 6px 136px;
+  background-color: #fcfcfc;
+  box-shadow: 0px 2px 10px rgba(109, 109, 109, 0.25), inset 0px 1px 0px rgba(255, 255, 255, 0.3);
+
+  div {
+    width: 113px;
+  }
+`
